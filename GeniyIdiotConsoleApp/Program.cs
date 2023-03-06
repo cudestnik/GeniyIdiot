109af92fca9623c747a3828228b43056ceb2f170
cudestnik <cash2000@bk.ru>
2023-03-06 12:56:14 +0300

    Создаю проект и написал код 1 урока.

--- GeniyIdiotConsoleApp/Program.cs
+++ GeniyIdiotConsoleApp/Program.cs
@@ -1,24 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace GeniyIdiotConsoleApp
 {
     class Program
-    {
-        static void Main(string[] args)
+    {  
+        static string[] GetQuestions(int countQuestions)
         {
-            string[] questions = new string[5];
+            string[] questions = new string[countQuestions];
 
             questions[0] = "Сколько будет два плюс два умноженное на два?";
             questions[1] = "Бревно нужно распилить на 10 частей,сколько надо сделать распилов?";
             questions[2] = "На двух руках 10 пальцев.Сколько пальцев на 5 руках?";
             questions[3] = "Укол делают каждые полчаса,сколько нужно минут для трёх уколов?";
             questions[4] = "Пять свечей горело,две потухли.Сколько осталось?";
+                return questions;
+        }
 
-            int[] answers = new int[5];
+        static void Main(string[] args)
+        {
+            int countQuestions = 5;
+            string[] questions = GetQuestions(countQuestions);
+            int[] answers = new int[countQuestions];
 
             answers[0] = 6;
             answers[1] = 9;
@@ -26,15 +28,77 @@ namespace GeniyIdiotConsoleApp
             answers[3] = 60;
             answers[4] = 2;
 
-          
+            int CountRightAnswers = 0;
 
-            for(int i = 0; i < questions.Length; i++) 
+            Random random = new Random();
+
+            for(int i = 0; i < countQuestions ; i++) 
             {
-                Console.WriteLine(questions[i]);
+                Console.WriteLine("Вопрос N" + (i+1));
+
+                int randomQuestionIndex = random.Next(0, countQuestions);
+                Console.WriteLine(questions[randomQuestionIndex]);
+
+                int userAnswer = Convert.ToInt32(Console.ReadLine());
+
+                int rightAnswer = answers[randomQuestionIndex];
+              
+
+                if(userAnswer == rightAnswer)
+                {
+                    CountRightAnswers++;
+                }               
             }
 
+            Console.WriteLine("Количество правильных ответов: " + CountRightAnswers);
 
+            /*string diagnose = "";
 
+            if(CountRightAnswers == 0) 
+            { 
+                diagnose = "Идиот";
+            }
+            if (CountRightAnswers == 1)
+            {
+                diagnose = "Кретин";
+            }
+            if (CountRightAnswers == 2) // Или эта конструкция или switch но она красивее и короче
+            {
+                diagnose = "Дурак";
+            }
+            if (CountRightAnswers == 3)
+            {
+                diagnose = "Нормальный";
+            }
+            if (CountRightAnswers == 4)
+            {
+                diagnose = "Талант";
+            }
+            if (CountRightAnswers == 5)
+            {
+                diagnose = "Гений";
+            }*/
+           /* switch(CountRightAnswers)
+            {
+                case 0: diagnose = "Идиот";break;
+                case 1: diagnose = "Кретин";break;
+                case 2: diagnose = "Дурак";break;           // вместо него заменили на массив diagnoses
+                case 3: diagnose = "Нормальный";break;
+                case 4: diagnose = "Талант";break;
+                case 5: diagnose = "Гений";break;
+
+            }
+           */
+
+            string[] diagnoses = new string[6];
+            diagnoses[0] = "Идиот";
+            diagnoses[1] = "Кретин";
+            diagnoses[2] = "Дурак";
+            diagnoses[3] = "Нормальный";
+            diagnoses[4] = "Талант";
+            diagnoses[5] = "Гений";
+
+            Console.WriteLine("Ваш диагноз: " + diagnoses[CountRightAnswers]);
         }
     }
 }
